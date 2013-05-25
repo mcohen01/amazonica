9021926229bebb99c9b3847fbd6778cac21a9736
Michael Cohen <mcohen01@gmail.com>
2013-05-25 15:18:17 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amazonica "0.1.6"
+(defproject amazonica "0.1.7"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"

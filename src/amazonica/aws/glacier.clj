ef283b222f5253fe3b5618be961a0dd2e2c6e642
Michael Cohen <mcohen01@gmail.com>
2013-09-28 16:54:50 -0700

    fix for Issue #18

--- src/amazonica/aws/glacier.clj
+++ src/amazonica/aws/glacier.clj
@@ -1,7 +1,8 @@
 (ns amazonica.aws.glacier
-  (:use [amazonica.core :only (set-client to-file)]
+  (:use [amazonica.core :only (get-credentials set-client to-file)]
         [robert.hooke :only (add-hook)])
-  (:import [com.amazonaws.services.glacier
+  (:import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+           [com.amazonaws.services.glacier
               AmazonGlacierClient]
            [amazonica TreeHash]
            [java.io
@@ -13,22 +14,33 @@
 (defn- file-hash->map
   [file]
   {:content-length (.length file)
-   :checksum (-> file
-                 TreeHash/computeSHA256TreeHash
-                 TreeHash/toHex)
-   :body (-> file
-             (FileInputStream.) 
-             (BufferedInputStream.))})
+   :checksum       (-> file
+                       TreeHash/computeSHA256TreeHash
+                       TreeHash/toHex)
+   :body           (-> file
+                       (FileInputStream.) 
+                       (BufferedInputStream.))})
+
+(defn parse-args
+  "Legacy support means credentials may or may not be passed
+   as the first argument."
+  [cred args]
+  (if (instance? DefaultAWSCredentialsProviderChain (get-credentials cred))
+      {:args (conj args cred)}
+      {:args args :cred cred}))
 
 (defn tree-hash
   [f cred & args]
-  (let [m    (apply hash-map args)
-        file (to-file (:body m))        
-        mm   (merge-with               
-               (fn [_ e] e)
-               m
-               (file-hash->map file))
-        rval (interleave (keys mm) (vals mm))]
-    (apply (partial f cred) rval)))
+  (let [arg-map (parse-args cred args)
+        func    (if (contains? arg-map :cred)
+                    (partial f cred)
+                    f)
+        m       (apply hash-map (:args arg-map))
+        file    (to-file (:body m))        
+        mm      (merge-with               
+                  (fn [_ e] e)
+                  m
+                  (file-hash->map file))]
+    (apply func (mapcat identity mm))))
 
 (add-hook #'upload-archive #'tree-hash)

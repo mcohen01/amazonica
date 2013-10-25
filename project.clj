81414649d151944bb2d9ee49fad4a5c508f15eaa
Michael Cohen <mcohen01@gmail.com>
2013-10-25 09:56:52 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject amazonica "0.1.25"
+(defproject amazonica "0.1.26"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src/main/java"]
-  :target-path "target"  
+  :target-path "target"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.amazonaws/aws-java-sdk "1.6.3"]
                  [org.clojure/algo.generic "0.1.0"]
                  [joda-time "2.1"]
+                 [cheshire "5.2.0"]
                  [robert/hooke "1.3.0"]])

610eac0a627e900d4ad2bde80199f5b45b12cf4a
glangford <glenn.langford@gmail.com>
2014-08-11 19:34:17 -0400

    Enhance mockRecord getData and add tests

--- test/amazonica/test/kinesisasync.clj
+++ test/amazonica/test/kinesisasync.clj
@@ -1,6 +1,7 @@
 (ns amazonica.test.kinesisasync
   (:use [clojure.test]
         [amazonica.aws.kinesis :as kinesis]
+        [taoensso.nippy :as nippy]
         [clojure.core.async :refer [chan close! <!! >!! sliding-buffer]])
   (:import [com.amazonaws.services.kinesis.clientlibrary.interfaces
               IRecordProcessor
@@ -18,14 +19,14 @@
            java.nio.ByteBuffer))
 
 ; com.amazonaws.services.kinesis.model Record
-(defn mockRecord [data]
+(defn mockRecord [seq data]
   (proxy [Record] []
     (getSequenceNumber []
-      (str data))
+      (str seq))
     (getPartitionKey []
-      (str data))
+      nil)
     (getData [] 
-      (java.nio.ByteBuffer/wrap (byte-array 8 [data])))))
+      (java.nio.ByteBuffer/wrap (nippy/freeze data )))))
 
 ; While Kinesis would store checkpointed sequence numbers in DynamoDB, 
 ; the mock stores this state in atom cp-state so it can be observed.
@@ -59,7 +60,7 @@
         next-check (atom 0)                 ; time of next checkpoint (ms)
         cp-interval 0.000001                ; time between checkpoint intervals (seconds)
         cp-state (atom -1)                  ; value of latest checkpointed sequence number
-        factory (#'kinesis/processor-factory-async shard-chan cp-chan identity cp-interval next-check)
+        factory (#'kinesis/processor-factory-async shard-chan cp-chan unwrap cp-interval next-check)
         processor (.createProcessor factory)
         cp-mock (mockCheckpointer cp-state)
         cp-mock-throttled (mockCheckpointer-throttled cp-state)]
@@ -67,21 +68,26 @@
         ; Simulate app checkpoint of sequence number 0
         (>!! cp-chan 0)
         ; Simulate incoming data from Kinesis, which will be written to shard-chan
-        (let [records (java.util.ArrayList. [(mockRecord 1) (mockRecord 2)]) ]
+        (let [records (java.util.ArrayList. [(mockRecord 1 "data1") (mockRecord 2 2) (mockRecord 3 [3 6 9])]) ]
           (.processRecords processor records cp-mock-throttled))
         ; Verify sequence number 0 was checkpointed as a side effect of processRecords
         (is (= @cp-state "0"))
         ; Simulate app reading from the buffered shard channel
         (let [d1 (<!! shard-chan)
-              d2 (<!! shard-chan)]
-              ; Verify the sequence numbers received on the channel
+              d2 (<!! shard-chan)
+              d3 (<!! shard-chan)]
+              ; Verify the sequence numbers and data received on the channel
               (is (= (:sequence-number d1) "1"))
-              (is (= (:sequence-number d2) "2")))
-        ; Simulate completion of processing by writing sequence #2 to the checkpoint channel
-        (>!! cp-chan 2)
+              (is (= (:data d1) "data1"))
+              (is (= (:sequence-number d2) "2"))
+              (is (= (:data d2) 2))
+              (is (= (:sequence-number d3) "3"))
+              (is (= (:data d3) [3 6 9])))
+        ; Simulate completion of processing by writing sequence #3 to the checkpoint channel
+        (>!! cp-chan 3)
         ; Simulate termination of processing initiated by Kinesis
         (.shutdown processor cp-mock ShutdownReason/TERMINATE)
-        ; Verify sequence number 2 was checkpointed as a side effect of termination
-        (is (= @cp-state "2"))
+        ; Verify sequence number 3 was checkpointed as a side effect of termination
+        (is (= @cp-state "3"))
     ))
 

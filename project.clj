d34f70a9f4768714e1f407087995be256401e92c
Michael Cohen <mcohen01@gmail.com>
2014-11-26 13:09:49 -0800

    setup leiningen deployment script

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amazonica "0.2.30"
+(defproject amazonica "0.2.4-SNAPSHOT"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"

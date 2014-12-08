e97ccf330f488c5b3f8cae91ca659d2afc21910d
Michael Cohen <mcohen01@gmail.com>
2014-12-08 13:55:13 -0800

    convenience form for PortRanges

--- src/amazonica/aws/ec2.clj
+++ src/amazonica/aws/ec2.clj
@@ -1,7 +1,7 @@
 (ns amazonica.aws.ec2
   (:require [amazonica.core :as amz])
   (:import com.amazonaws.services.ec2.AmazonEC2Client
-           com.amazonaws.services.ec2.model.Tag))
+           [com.amazonaws.services.ec2.model PortRange Tag]))
 
 (defn- key->str [kw]
   (if (keyword? kw) 
@@ -21,7 +21,30 @@
       (if (sequential? value)
           (do (.setKey tag (-> value first key->str))
               (.setValue tag (-> value second str))))
-      tag)))
-              
+      tag))
+  PortRange
+  (fn [value]
+    (let [ports (PortRange.)]
+      (cond
+        (string? value)
+        (do
+          (.setFrom ports (Integer. value))
+          (.setTo ports (Integer. value)))
+        (number? value)
+        (do
+          (.setFrom ports (int value))
+          (.setTo ports (int value)))
+        (and (map? value) (:from value) (:to value))
+        (do
+          (.setFrom ports (int (:from value)))
+          (.setTo ports (int (:to value))))
+        (and (sequential? value) (or (= 1 (count value)) (= 2 (count value))))
+        (do
+          (.setFrom ports (int (first value)))
+          (.setTo ports (int (last value))))
+        :default
+        (throw (IllegalArgumentException.
+                 (format "Don't know how to create PortRange from %s" value))))
+      ports)))
 
 (amz/set-client AmazonEC2Client *ns*)

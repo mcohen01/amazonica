63176b827e35a6b702afb87f376fd3a58e826595
Michael Cohen <mcohen01@gmail.com>
2014-12-15 19:04:06 -0800

    bump SDK dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amazonica "0.3.5"
+(defproject amazonica "0.3.6"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/algo.generic "0.1.2"]
-                 [com.amazonaws/aws-java-sdk "1.9.7" :exclusions [joda-time]]
+                 [com.amazonaws/aws-java-sdk "1.9.11" :exclusions [joda-time]]
                  [org.apache.httpcomponents/httpclient "4.3.3"]
                  [com.amazonaws/amazon-kinesis-client "1.1.0" :exclusions [joda-time]]
                  [joda-time "2.2"]

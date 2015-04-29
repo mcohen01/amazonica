4099a582fdd0d088dd973f08febfefdeaf0b40d2
Michael Cohen <mcohen01@gmail.com>
2015-04-29 16:09:53 -0700

    Update ecs.clj

--- src/amazonica/aws/ecs.clj
+++ src/amazonica/aws/ecs.clj
@@ -1,7 +1,5 @@
 (ns amazonica.aws.ecs
   (:require [amazonica.core :as amz])
-  (:import [com.amazonaws.services.ecs AmazonECS AmazonECSClient]))
+  (:import [com.amazonaws.services.ecs AmazonECSClient]))
 
 (amz/set-client AmazonECSClient *ns*)
-
-

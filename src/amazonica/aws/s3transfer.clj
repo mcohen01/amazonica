c418f6e027c958fe56b3c6c59e67a5230e3514b5
Michael Cohen <mcohen01@gmail.com>
2015-06-02 12:29:47 -0700

    Fix for Issue #138
    After the recent Java SDK upgrade, the getRawMetadataValue() method was added to ObjectMetadata. This method can't be resolved via the simple getter reflection Amazonica uses. The fix is to simply include the s3 namespace from s3transfer, which will result in ObjectMetadata being marshalled without calling that method.
    
    In addition, it looks like a few of the methods on the Download object now require that the download is completed before being invoked, otherwise they throw NPEs.

--- src/amazonica/aws/s3transfer.clj
+++ src/amazonica/aws/s3transfer.clj
@@ -1,5 +1,6 @@
 (ns amazonica.aws.s3transfer
-  (:use [amazonica.core :only (IMarshall marshall coerce-value stack->string)])
+  (:require [amazonica.core :refer [IMarshall marshall coerce-value stack->string]]
+            [amazonica.aws.s3])
   (:import [com.amazonaws.event
             ProgressEvent
             ProgressListener]
@@ -25,10 +26,17 @@
       (.addProgressListener obj listener)
       listener)))
 
+(defmacro wait
+  [transfer & body]
+  `(fn []
+     (.waitForCompletion ~transfer)
+     (do ~@body)))
+
 (defn transfer
   [obj]
-  {:add-progress-listener    (add-listener obj)
-   :get-description          #(.getDescription obj)
+  {:transfer                 obj
+   :add-progress-listener    (add-listener obj)
+   :get-description          (wait obj (.getDescription obj))
    :get-progress             #(marshall (.getProgress obj))
    :get-state                #(str (.getState obj))
    :is-done                  #(.isDone obj)
@@ -58,10 +66,9 @@
   (marshall [obj]
     (let [t (transfer obj)]
       ((:add-progress-listener t) (partial default-listener t))
-      (merge (transfer obj)
-             {:key             #(.getKey obj)
-              :bucket-name     #(.getBucketName obj)
-              :object-metadata #(marshall (.getObjectMetadata obj))})))
+      (merge t {:key             (wait obj (.getKey obj))
+                :bucket-name     (wait obj (.getBucketName obj))
+                :object-metadata (wait obj (marshall (.getObjectMetadata obj)))})))
   
   MultipleFileUpload
   (marshall [obj]

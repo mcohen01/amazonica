820901640ead5114c60cd3989554c7969a417b24
Michael Cohen <mcohen01@gmail.com>
2015-06-11 15:50:40 -0700

    fix for issue #139

--- src/amazonica/aws/opsworks.clj
+++ src/amazonica/aws/opsworks.clj
@@ -1,5 +1,13 @@
 (ns amazonica.aws.opsworks
   (:require [amazonica.core :as amz])
-  (:import com.amazonaws.services.opsworks.AWSOpsWorksClient))
+  (:import com.amazonaws.services.opsworks.AWSOpsWorksClient
+           com.amazonaws.services.opsworks.model.DeploymentCommand))
+
+(amz/register-coercions
+  DeploymentCommand
+  (fn [value]
+    (doto (DeploymentCommand.)
+      (.setName (:name value))
+      (.setArgs (:args value)))))
 
 (amz/set-client AWSOpsWorksClient *ns*)

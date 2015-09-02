f3dba4b382cc25d8abd185d1134ed766e518ad00
Michael Cohen <mcohen01@gmail.com>
2015-09-02 14:26:20 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amazonica "0.3.31"
+(defproject amazonica "0.3.32"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"
@@ -7,10 +7,10 @@
   :javac-options ["-target" "1.6" "-source" "1.6"]
   :target-path "target"
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/algo.generic "0.1.2"]
                  [com.amazonaws/aws-java-sdk "1.10.12" :exclusions [joda-time]]
                  [com.amazonaws/amazon-kinesis-client "1.1.0" :exclusions [joda-time]]
-                 [joda-time "2.2"]
+                 [joda-time "2.8.1"]
                  [robert/hooke "1.3.0"]
                  [com.taoensso/nippy "2.7.0"]])

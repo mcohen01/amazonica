b4287d18422caa8d52a2b2d47d485f857deb51e4
Pramod Somashekar <psomashekar@mdsol.com>
2017-01-11 10:53:23 -0500

    adding stepfunctions support

---
+++ src/amazonica/aws/stepfunctions.clj
@@ -0,0 +1,25 @@
+(ns amazonica.aws.stepfunctions
+  (:use [amazonica.core :only (kw->str parse-args)]
+        [clojure.walk]
+        [robert.hooke :only (add-hook)])
+  (:import [com.amazonaws.services.stepfunctions AWSStepFunctionsClient]))
+
+(amazonica.core/set-client AWSStepFunctionsClient *ns*)
+
+(defn get-activity-task-result [arn]
+  (get-activity-task {:activityArn arn})
+  )
+
+(defn mark-task-success [token output]
+  (send-task-success {:output output
+                      :taskToken token})
+  )
+
+(defn mark-task-failure [token]
+  (send-task-failure {:taskToken token})
+  )
+
+(defn start-state-machine [input state-machine-arn]
+  (start-execution {:stateMachineArn state-machine-arn
+                    :input input})
+  )

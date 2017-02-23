604d8eb767e98e43e9c2a929d98c49ee09a6af90
lvh <_@lvh.io>
2017-02-23 16:38:42 -0600

    Add a list of proposed changes

--- test/amazonica/test/core.clj
+++ test/amazonica/test/core.clj
@@ -24,6 +24,8 @@
              elastictranscoder
              glacier
              identitymanagement
+             iot
+             iotdata
              kinesis
              kms
              lambda
@@ -55,3 +57,49 @@
     (is (= :abc-def (c->k "AbcDef")))
 
     (is (= :abc-def (c->k "ABCDef")))))
+
+(def changed-camel->keyword-vars
+  "These are vars that were converted to kebab-case in a way that didn't deal
+  with acronyms (any sequence of capital letters, but also some things like
+  iSCSI and OpenID) correctly. This exists so that we can verify that we didn't
+  accidentally break backwards compatibility.
+
+  This is a collection of strings and symbols and not just vars so we can verify
+  other information about the two resulting vars; and that you get a nice test
+  failure instead of an ugly compilation error when it fails."
+  {"identitymanagement" [['list-mfadevices
+                          'list-mfa-devices]
+                         ['list-open-idconnect-providers
+                          'list-openid-connect-providers]
+                         ['list-samlproviders
+                          'list-saml-providers]
+                         ['list-sshpublic-keys
+                          'list-ssh-public-keys]
+                         ['list-virtual-mfadevices
+                          'list-virtual-mfa-devices]]
+   "iot" [['describe-cacertificate
+           'describe-ca-certificate]
+          ['list-cacertificates
+           'list-ca-certificates]]
+   "rds" [['describe-dbcluster-parameter-groups
+           'describe-db-cluster-parameter-groups]
+          ['describe-dbcluster-snapshots
+           'describe-db-cluster-snapshots]
+          ['describe-dbclusters
+           'describe-db-clusters]
+          ['describe-dbengine-versions
+           'describe-db-engine-versions]
+          ['describe-dbinstances
+           'describe-db-instances]
+          ['describe-dbparameter-groups
+           'describe-db-parameter-groups]
+          ['describe-dbsecurity-groups
+           'describe-db-security-groups]
+          ['describe-dbsnapshots
+           'describe-db-snapshots]
+          ['describe-dbsubnet-groups
+           'describe-db-subnet-groups]]
+   "storagegateway" [['describe-storedi-scsivolumes
+                      'describe-stored-iscsi-volumes]
+                     ['describe-cachedi-scsivolumes
+                      'describe-cached-iscsivolumes]]})

1a6f2f227fb849cf683e00b5ba35b17304ca03b4
Brian Jenkins <bonkydog@bonkydog.com>
2017-04-01 12:57:46 -0700

    Convert LogConfiguration options to the string-keyed map it expects

--- src/amazonica/aws/ecs.clj
+++ src/amazonica/aws/ecs.clj
@@ -1,5 +1,15 @@
 (ns amazonica.aws.ecs
-  (:require [amazonica.core :as amz])
-  (:import [com.amazonaws.services.ecs AmazonECSClient]))
+  (:require [amazonica.core :as amz]
+            [clojure.walk :as walk])
+  (:import [com.amazonaws.services.ecs AmazonECSClient]
+           (com.amazonaws.services.ecs.model LogConfiguration)))
 
 (amz/set-client AmazonECSClient *ns*)
+
+(amz/register-coercions
+  LogConfiguration
+  (fn [value]
+    (doto (LogConfiguration.)
+      (.setLogDriver (:log-driver value))
+      (.setOptions (walk/stringify-keys (:options value))))))
+

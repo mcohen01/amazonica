24844c2ebd2e91eb4ef323e6c0d9c5142dc577f7
Michael Cohen <mcohen01@gmail.com>
2017-05-09 07:18:21 -0700

    version bump

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A comprehensive Clojure client for the entire [Amazon AWS api][1].
 
 Leiningen coordinates:
 ```clj
-[amazonica "0.3.97"]
+[amazonica "0.3.98"]
 ```
 
 For Maven users:
@@ -26,7 +26,7 @@ and the following dependency:
 <dependency>
   <groupId>amazonica</groupId>
   <artifactId>amazonica</artifactId>
-  <version>0.3.97</version>
+  <version>0.3.98</version>
 </dependency>
 ```
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amazonica "0.3.97"
+(defproject amazonica "0.3.98"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
   :url "https://github.com/mcohen01/amazonica"
   :license {:name "Eclipse Public License"
@@ -9,9 +9,14 @@
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/algo.generic "0.1.2"]
-                 [com.amazonaws/aws-java-sdk "1.11.109" :exclusions [joda-time]]
-                 [com.amazonaws/amazon-kinesis-client "1.7.4" :exclusions [joda-time]]
-                 [com.amazonaws/dynamodb-streams-kinesis-adapter "1.2.1" :exclusions [joda-time]]
+                 [com.amazonaws/aws-java-sdk "1.11.115" :exclusions [joda-time]]
+                 [com.amazonaws/amazon-kinesis-client "1.7.5" :exclusions [joda-time]]
+                 [com.amazonaws/dynamodb-streams-kinesis-adapter "1.2.1"
+                  :exclusions [com.amazonaws/amazon-kinesis-client
+                               com.amazonaws/aws-java-sdk-cloudwatch
+                               com.amazonaws/aws-java-sdk-dynamodb
+                               com.amazonaws/aws-java-sdk-kinesis
+                               joda-time]]
                  [joda-time "2.9.6"]
                  [robert/hooke "1.3.0"]
                  [com.taoensso/nippy "2.12.2"]])

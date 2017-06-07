e16f6cd6a5ee4b9006d3db6ad815c9a855bbd77a
Brenden <brenden.doyle@gmail.com>
2017-06-07 15:05:56 -0300

    Support sending binary data over SNS

--- src/amazonica/aws/sns.clj
+++ src/amazonica/aws/sns.clj
@@ -1,7 +1,8 @@
 (ns amazonica.aws.sns
   (:require [amazonica.core :as amz])
   (:import com.amazonaws.services.sns.AmazonSNSClient
-           [com.amazonaws.services.sns.model MessageAttributeValue]))
+           [com.amazonaws.services.sns.model MessageAttributeValue]
+           [java.nio ByteBuffer]))
 
 (amz/register-coercions
   MessageAttributeValue
@@ -9,6 +10,7 @@
     (cond
       (string? value) (doto (MessageAttributeValue.) (.withDataType "String") (.withStringValue value))
       (number? value) (doto (MessageAttributeValue.) (.withDataType "Number") (.withStringValue (str value)))
+      (instance? ByteBuffer value) (doto (MessageAttributeValue.) (.withDataType "Binary") (.withBinaryValue value))
       :else (throw (ex-info
                      (format "Values of type %s are not supported for SNS Message Attributes" (class value))
                      {:value value})))))
